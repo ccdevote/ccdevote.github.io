e73fec0ba981a2cc6963f6f6ed92c07d14f01cfd
Zhiyong Ma <ccdevote@gmail.com>
2014-04-04 18:46:43 +0800

    fix some style bug and add disqus

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@
  * Created by mzy on 14-4-2.
  */
 $(document).ready(function(){
+    var post= $("#post");
+    var postImg = $("#post img");
+    console.log(post.width()*0.8);
+    postImg.addClass("img-rounded").width(parseInt(post.width()*0.8));
+    postImg.css({
+       "margin-left":post.width()*0.1
+    });
+    $(window).resize(function(){
+        postImg.width(parseInt(post.width())*0.8);
+        postImg.css({
+            "margin-left":parseInt(post.width())*0.1
+        });
+    });
+
+  // Top Bar
   $('#goto_top_btn').click(function() {
         var s = $(window).scrollTop(),h = $(window).height();
         if (s > h * 2) {
